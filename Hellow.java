e076e87f58cf7ffad3c8c4443b329d4ae429c442
arnab <arnablink7@gmail.com>
2023-02-06 19:19:50 +0530

    Relational operator

--- Hellow.java
+++ Hellow.java
@@ -1,11 +1,11 @@
 public class Hellow{
 public static void main(String[] args) {
 
-   int a = 47;
-   int b = 33;
-  // int c = a++;  // post-increment
-   int c = --a;  // pre-increment
-   System.out.println(c);
+int a = 12;
+
+int b = 7;
+boolean  result = a > b; // relational operator are always boolean value
+System.out.println(result);
    
 
 }
