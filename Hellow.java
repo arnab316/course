5fd668d5fb3304b2bca7a40f2a7e8e99e3a70f5a
Arnab Golder <arnabgolder7@gmail.com>
2023-02-06 20:43:59 +0530

    logical operator

--- Hellow.java
+++ Hellow.java
@@ -2,9 +2,13 @@ public class Hellow{
 public static void main(String[] args) {
 
 int a = 12;
-
 int b = 7;
-boolean  result = a > b; // relational operator are always boolean value
+
+int x = 10;
+int y = 9;
+
+boolean result = a>b || x < y;
+
 System.out.println(result);
    
 
