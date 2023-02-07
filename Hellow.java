71665ef7f9baf8fa53f643ba2e49e4783028d5b4
Arnab Golder <arnabgolder7@gmail.com>
2023-02-07 21:04:21 +0530

    objects-2

--- Hellow.java
+++ Hellow.java
@@ -5,10 +5,14 @@
 }
 public class Hellow{
 public static void main(String[] args) {
+   char c = 'a';
 
    int x=7;
    int y=10;
+   // objects
    calculator cal = new calculator();
  System.out.println(cal.add(x,y));
+
+
 
 }}
