671a36d20d7be1d7232152cab7bb063e7de82b37
Arnab Golder <arnabgolder7@gmail.com>
2023-02-07 20:15:45 +0530

    loops

--- Hellow.java
+++ Hellow.java
@@ -1,27 +1,36 @@
 public class Hellow{
 public static void main(String[] args) {
+//loops
 
-    int i = 5;
-    switch(i) {
-     case 1:
-    System.out.println("Monday");
-    break;
-    case 2:
-    System.out.println("Tuesday");
-    break;
-    case 3:
-    System.out.println("Wednesday");
-    break;
-    case 4:
-    System.out.println("Thursday"); 
-    break;
-    case 5:
-    System.out.println("Friday");
-        break;
-    default:
-    System.out.println("Enter a valid day");
+// repetitive any string four times
+
+/*  for loop example
+for (int i=1; i<5; i++){
+    System.out.println(i);
 }
+*/
+
+/*  while loop example
+int i=1;
 
 
+while (i<=5){
+    System.out.println("hi");
+    i++;
+
 }
-}
+*/
+
+//do-while loop example
+
+int i=10;
+do
+{
+    System.out.println("Hellow");
+    i++;
+
+}while(i<=5);
+
+
+
+}}
