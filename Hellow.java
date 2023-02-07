165b117da48f00c3fd1866517fec70e82514b9d2
Arnab Golder <arnabgolder7@gmail.com>
2023-02-07 20:50:23 +0530

    Objects in java

--- Hellow.java
+++ Hellow.java
@@ -1,36 +1,14 @@
+ class calculator {
+    public int add(int a, int b) {
+      return a + b;
+    }   
+}
 public class Hellow{
 public static void main(String[] args) {
-//loops
 
-// repetitive any string four times
-
-/*  for loop example
-for (int i=1; i<5; i++){
-    System.out.println(i);
-}
-*/
-
-/*  while loop example
-int i=1;
-
-
-while (i<=5){
-    System.out.println("hi");
-    i++;
-
-}
-*/
-
-//do-while loop example
-
-int i=10;
-do
-{
-    System.out.println("Hellow");
-    i++;
-
-}while(i<=5);
-
-
+   int x=7;
+   int y=10;
+   calculator cal = new calculator();
+ System.out.println(cal.add(x,y));
 
 }}
