b8fcbe50c29d88ee9ad133f79eb7cd46626c0ae3
Arnab Golder <arnabgolder7@gmail.com>
2023-02-08 20:09:07 +0530

    Array

--- Hellow.java
+++ Hellow.java
@@ -1,26 +1,17 @@
-class computer{
-  public String getMeApen(int cost)
-{
-    if(cost>= 10)
-{
-    return "pen";
-}
-else{ return "Nothing";}
-}
-  public void music(){
-     System.out.println("playing music");
-
-}
-}
-
-
 public class Hellow{
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
+int arr1[] ={55,78,94,44};
+// System.out.println(arr1[3]);
+for(int i =0; i<arr1.length; i++){
+  System.out.print(arr1[i]);
+}
 
-  computer obj = new computer();
-   obj.music();
-   String str = obj.getMeApen(1);
-     System.out.println(str);
+
+int num[]= new int [2];
+num[0]=41;
+num[1]=13;
+// System.out.println(num[0]); 
 
 }
 }
