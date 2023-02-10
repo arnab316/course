2818d8f0360d6ae558647ff9b206ac5d00bddc0b
Arnab Golder <arnabgolder7@gmail.com>
2023-02-10 13:26:16 +0530

    Array of objects

--- Hellow.java
+++ Hellow.java
@@ -1,20 +1,39 @@
+class Student{
+  int rollno;
+  String name;
+  int marks;
+
+}
 public class Hellow{
 public static void main(String[] args) 
 {
-
-int num1 [][] = new int [3][4];
-  //  num1 [0][2] = 6;
-  //  num1 [1][0] = 3;
-  //  num1 [2][3] = 2;
-  //  System.out.println(num1 [1][0]);
- for(int i=0;i<3;i++){
-  for(int j=0;j<4;j++){
-    num1[i][j] = (int)(Math.random() * 10);
-
-    System.out.print(num1[i][j]);
+   
+    Student s1=new Student();
+    s1.rollno=101;
+    s1.name="Ravi";
+    s1.marks=90;
+    
+    Student s2=new Student();
+    s2.rollno=102;
+    s2.name="rogan";
+    s2.marks=80;
+    Student s3 = new Student();
+    s3.rollno=103;  
+    s3.name="alex";
+    s3.marks=70;  
+     // System.out.println(s3); // garbage value we get
+   Student students[] = new Student[3];
+   students[0] = s1;
+   students[1] = s2;
+   students[2] = s3;
+   
+ 
+  for (int i=0; i<students.length; i++)
+  {
+    System.out.print(students[i].name + ": "+ students[i].marks);
   }
- }
-
+ 
+  
 
 
 }
