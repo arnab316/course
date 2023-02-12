5011f26a1e1582a74da61c2b0530cf09d422c56b
Arnab Golder <arnabgolder7@gmail.com>
2023-02-12 16:14:50 +0530

    this & constructor

--- Hellow.java
+++ Hellow.java
@@ -1,26 +1,49 @@
 class Human{
+
   private String name;
   private int  age ;
-
-  public String getName() {
-    return name;
-  }
-  public void setName(String n) {
-      name= n;  
+  
+public Human(){
+   name = "john";
+   age= 20;
 }
-  public int getAge() {
-    return age;
+
+
+
+public Human(String name, int age) {
+   this.name = name;
+   this.age = age;
+}
+
+
+
+public String getName() {
+   return name;
+}
+public void setName(String name) {
+   this.name = name;
+}
+public int getAge() {
+   return age;
+}
+public void setAge(int age) {
+   this.age = age;
+}
+
+
   
 }
-}
+
 public class Hellow
 {
 public static void main(String[] args) 
 {
+   Human h1=new Human();
+   //h1.setAge(19);      
+   //h1.setName("Ravi");
+   System.out.println(h1.getName()+ " "+h1.getAge());
 
-Human h1 = new Human();
-h1.setName("Arnab");
-      System.out.println(h1.getName());
+
 }
 }
 
