4838a3a55eff23eaaa54150d1743e10abc65949c
Arnab Golder <arnabgolder7@gmail.com>
2023-02-12 15:17:41 +0530

    encapsulation

--- Hellow.java
+++ Hellow.java
@@ -1,50 +1,26 @@
-class Mobile{
-  String brand;
-String  price;
-static String   name;
+class Human{
+  private String name;
+  private int  age ;
 
-// static block
-static{
-  name="Phone";
-  System.out.println("in static block");
+  public String getName() {
+    return name;
+  }
+  public void setName(String n) {
+      name= n;  
 }
- 
-
-//constractor
-
-public Mobile(){
-
-   brand = "";
-   price = "200";
-   System.out.println("im a constractor");
-
+  public int getAge() {
+    return age;
+  
 }
-   public void show(){
-  System.out.println(brand + " " + price + " " + name);
-
-   }
 }
 public class Hellow
 {
-public static void main(String[] args) throws ClassNotFoundException
+public static void main(String[] args) 
 {
 
-
-   Class.forName("Mobile");
-
-   // Mobile ob1 = new Mobile();
-   //   ob1.brand = "Apple";
-   //  ob1.price = "1000";
-   //  Mobile.name = "Smartphone";     
-
-   //  ob1.show();
-   //  Mobile ob2 = new Mobile();
-   //  ob2.brand = "Samsung";
-   // ob2.price = "700";
-   // Mobile.name = "Smartphone";
-   // ob2.show();
-   //  Mobile ob3 = new Mobile();
-
+Human h1 = new Human();
+h1.setName("Arnab");
+      System.out.println(h1.getName());
 }
 }
 
